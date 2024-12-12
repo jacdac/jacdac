60166c990ecd43439320e93437ef9950e9057227
Thomas Ball <thomasjball@users.noreply.github.com>
2024-12-12 10:29:35 -0800

    update microsoft/pxt-jacdac to jacdac/pxt-jacdac

--- spectool/jdspectool.ts
+++ spectool/jdspectool.ts
@@ -60,7 +60,7 @@ function toPxtJson(spec: jdspec.ServiceSpec) {
             supportedTargets: ["microbit", "arcade", "maker", "calliopemini"],
             dependencies: {
                 core: "*",
-                jacdac: "github:microsoft/pxt-jacdac",
+                jacdac: "github:jacdac/pxt-jacdac",
             },
         },
         null,
@@ -885,7 +885,7 @@ function processSpec(dn: string) {
         upgrades: Record<string, string>
     } = {
         packages: {
-            approvedRepos: ["microsoft/pxt-jacdac"],
+            approvedRepos: ["jacdac/pxt-jacdac"],
         },
         upgrades: {},
     }
@@ -905,7 +905,7 @@ function processSpec(dn: string) {
         )
         pxtJacdacVersion = pxtJacdacjson.version
         mkcTargetConfig.upgrades[
-            "microsoft/pxt-jacdac"
+            "jacdac/pxt-jacdac"
         ] = `min:v${pxtJacdacVersion}`
     }
 
@@ -958,7 +958,7 @@ function processSpec(dn: string) {
                 supportedTargets?: string[]
                 files: string[]
             } = JSON.parse(fs.readFileSync(mkcdpxtjson, { encoding: "utf8" }))
-            const repo = `microsoft/pxt-jacdac/${mkcdsrvdirname}`
+            const repo = `jacdac/pxt-jacdac/${mkcdsrvdirname}`
             mkcdServices.push({
                 service: json.shortId,
                 client: {
