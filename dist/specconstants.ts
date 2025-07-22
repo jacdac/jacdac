1cebfcfb991025c4583fddcc40229ac0dcb78cba
thomasjball <thomasjball@users.noreply.github.com>
2025-07-22 20:48:48 +0000

    Apply automatic changes

--- dist/specconstants.ts
+++ dist/specconstants.ts
@@ -7522,6 +7522,68 @@ export namespace RoverRegPack {
     export const Kinematics = "i16.16 i16.16 i16.16 i16.16 i16.16"
 }
 
+// Service RPM constants
+export const SRV_R_PM = 0x19f8e291
+export enum RPMReg {
+    /**
+     * Read-only i24.8 (int32_t). The RPM reading of the sensor.
+     *
+     * ```
+     * const [rpm] = jdunpack<[number]>(buf, "i24.8")
+     * ```
+     */
+    Rpm = 0x101,
+
+    /**
+     * Constant uint8_t. The amount of ticks for 1 full rotation.
+     *
+     * ```
+     * const [clicksPerTurn] = jdunpack<[number]>(buf, "u8")
+     * ```
+     */
+    ClicksPerTurn = 0x180,
+
+    /**
+     * Constant i24.8 (int32_t). Lowest rpm that can be reported.
+     *
+     * ```
+     * const [minRpm] = jdunpack<[number]>(buf, "i24.8")
+     * ```
+     */
+    MinRpm = 0x104,
+
+    /**
+     * Constant i24.8 (int32_t). Lowest rpm that can be reported.
+     *
+     * ```
+     * const [maxRpm] = jdunpack<[number]>(buf, "i24.8")
+     * ```
+     */
+    MaxRpm = 0x105,
+}
+
+export namespace RPMRegPack {
+    /**
+     * Pack format for 'rpm' data.
+     */
+    export const Rpm = "i24.8"
+
+    /**
+     * Pack format for 'clicks_per_turn' data.
+     */
+    export const ClicksPerTurn = "u8"
+
+    /**
+     * Pack format for 'min_rpm' data.
+     */
+    export const MinRpm = "i24.8"
+
+    /**
+     * Pack format for 'max_rpm' data.
+     */
+    export const MaxRpm = "i24.8"
+}
+
 // Service Satellite Navigation System constants
 export const SRV_SAT_NAV = 0x19dd6136
 export enum SatNavReg {
